efb75d0eb561bd1a07323791199512820f8bb5b3
obkwon <okwon78@gmail.com>
2018-11-26 21:30:08 +0900

    delete log and data dir

--- model/main.py
+++ model/main.py
@@ -15,69 +15,66 @@ def import_data():
     return input_data.read_data_sets(FLAGS.data_dir, fake_data=FLAGS.fake_data)
 
 
+def weight_variable(shape):
+    """Create a weight variable with appropriate initialization."""
+    return tf.get_variable(name='weights', initializer=tf.truncated_normal(shape=shape))
+
+
+def bias_variable(shape):
+    """Create a bias variable with appropriate initialization."""
+    return tf.get_variable(name='bias', initializer=tf.constant(0.1, shape=shape))
+
+
+def variable_summaries(name, tensors):
+    """Attach a log of summaries to a Tensor (for TensorBoard visualization)."""
+    mean = tf.reduce_mean(tensors)
+    tf.summary.scalar(name + '_mean', mean)
+    stddev = tf.sqrt(tf.reduce_mean(tf.square(tensors - mean)))
+    tf.summary.scalar(name + '_stddev', stddev)
+    tf.summary.scalar(name + '_max', tf.reduce_max(tensors))
+    tf.summary.scalar(name + '_min', tf.reduce_min(tensors))
+    tf.summary.histogram(name + '_histogram', tensors)
+
+
+def nn_layer(name, input_tensor, input_dim, output_dim, act, dropout_keep_prob=None):
+    with tf.variable_scope(name):
+        weights = weight_variable(shape=(input_dim, output_dim))
+        variable_summaries('weights', weights)
+        biases = bias_variable(shape=(output_dim,))
+        variable_summaries('biases', biases)
+        logits = tf.matmul(input_tensor, weights) + biases
+        tf.summary.histogram('logits', logits)
+        activations = act(logits, name='activation')
+        tf.summary.histogram('activations', activations)
+
+        if dropout_keep_prob is not None:
+            tf.summary.scalar('dropout_keep_probability', dropout_keep_prob)
+            return tf.nn.dropout(activations, dropout_keep_prob)
+        else:
+            return activations
+
+
 def train(data):
-    x = tf.placeholder(dtype=tf.float32, shape=(None, IMAGE_SIZE), name='x-input')
-    y_ = tf.placeholder(dtype=tf.int64, shape=(None,), name='y-input')
+    with tf.name_scope('input'):
+        x = tf.placeholder(dtype=tf.float32, shape=(None, IMAGE_SIZE), name='x-input')
+        y_ = tf.placeholder(dtype=tf.int64, shape=(None,), name='y-input')
+        image_shaped_input = tf.reshape(x, shape=(-1, 28, 28, 1))
+        tf.summary.image(name='images', tensor=image_shaped_input)
+        keep_prob = tf.placeholder(dtype=tf.float32, name='keep_prob')
 
-    image_shaped_input = tf.reshape(x, shape=(-1, 28, 28, 1))
-    tf.summary.image('imput', image_shaped_input)
-
-    def weight_variable(shape):
-        """Create a weight variable with appropriate initialization."""
-        return tf.get_variable(name='weights', initializer=tf.truncated_normal(shape=shape))
-
-    def bias_variable(shape):
-        """Create a bias variable with appropriate initialization."""
-        return tf.get_variable(name='bias', initializer=tf.constant(0.1, shape=shape))
-
-    def variable_summaries(tensors):
-        """Attach a log of summaries to a Tensor (for TensorBoard visualization)."""
-
-        with tf.name_scope('summaries'):
-            mean = tf.reduce_mean(tensors)
-            tf.summary.scalar('mean', mean)
-
-            with tf.name_scope('stddev'):
-                stddev = tf.sqrt(tf.reduce_mean(tf.square(tensors - mean)))
-                tf.summary.scalar('stddev', stddev)
-                tf.summary.scalar('max', tf.reduce_max(tensors))
-                tf.summary.scalar('min', tf.reduce_min(tensors))
-                tf.summary.histogram('histogram', tensors)
-
-    def nn_layer(layer_name, input_tensor, input_dim, output_dim, act, dropout_keep_prob=None):
-
-        with tf.variable_scope(layer_name):
-            with tf.name_scope(layer_name):
-                weights = weight_variable(shape=(input_dim, output_dim))
-                variable_summaries(weights)
-                biases = bias_variable(shape=(output_dim,))
-                variable_summaries(biases)
-                logits = tf.matmul(input_tensor, weights) + biases
-                tf.summary.histogram('logits', logits)
-                activations = act(logits, name='activation')
-                tf.summary.histogram('activations', activations)
-
-                if dropout_keep_prob is not None:
-                    tf.summary.scalar('dropout_keep_probability', keep_prob)
-                    return tf.nn.dropout(activations, keep_prob)
-                else:
-                    return activations
-
-    keep_prob = tf.placeholder(dtype=tf.float32)
-
-    hidden1 = nn_layer(layer_name='hidden1',
+    hidden1 = nn_layer(name='first',
                        input_tensor=x,
                        input_dim=IMAGE_SIZE, output_dim=500,
                        act=tf.nn.relu,
                        dropout_keep_prob=None)
 
-    hidden2 = nn_layer(layer_name='hidden2',
+    hidden2 = nn_layer(name='second',
                        input_tensor=hidden1,
                        input_dim=500, output_dim=300,
                        act=tf.nn.relu,
                        dropout_keep_prob=keep_prob)
 
-    y = nn_layer(layer_name='final_layer',
+    y = nn_layer(name='last',
                  input_tensor=hidden2,
                  input_dim=300, output_dim=10,
                  act=tf.identity,
@@ -91,11 +88,9 @@ def train(data):
         train_step = tf.train.AdamOptimizer(FLAGS.learning_rate).minimize(cross_entropy)
 
     with tf.name_scope('accuracy'):
-        with tf.name_scope('correct_prediction'):
-            correct_prediction = tf.equal(tf.argmax(y, 1), y_)
-        with tf.name_scope('accuracy'):
-            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-            tf.summary.scalar('accuracy', accuracy)
+        correct_prediction = tf.equal(tf.argmax(y, 1), y_)
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        tf.summary.scalar('accuracy', accuracy)
 
     def feed_dict(train):
 
@@ -140,11 +135,15 @@ def train(data):
         train_writer.close()
         test_writer.close()
 
+    for name in [n.name for n in tf.get_default_graph().as_graph_def().node]:
+        print(name)
+
 
 def main(_):
     logging.basicConfig(filename='kt.log', level=logging.ERROR)
 
     if tf.gfile.Exists(FLAGS.log_dir):
+        print('Delete previous log dir')
         tf.gfile.DeleteRecursively(FLAGS.log_dir)
     tf.gfile.MakeDirs(FLAGS.log_dir)
 
@@ -155,7 +154,9 @@ def main(_):
     train(data)
 
 
-if __name__ is '__main__':
+if __name__ == '__main__':
+    print('start running')
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--fake_data',
                         type=bool,
